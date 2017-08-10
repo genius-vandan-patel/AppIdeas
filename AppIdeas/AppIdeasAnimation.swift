34ccb556e8390bb88908eed2fdd0b34bd03f5a25
Main User <VandanPatel@Mains-MacBook-Pro.local>
2017-08-10 10:16:01 -0400

    Added Keyboard Manager for whenever keyboard shows on the screen.

--- AppIdeas/AppIdeasAnimation.swift
+++ AppIdeas/AppIdeasAnimation.swift
@@ -23,15 +23,45 @@ class AppIdeasAnimation {
     
     func fadeInAnimation(_ view: UIView, delay: Double, duration: Double) {
         view.alpha = 0.0
-        UIView.animate(withDuration: duration, delay: delay, options: .allowUserInteraction, animations: { 
+        UIView.animate(withDuration: duration, delay: delay, options: .allowUserInteraction, animations: {
             view.alpha = 1.0
         }, completion: nil)
     }
     
     func scaleInAnimation(_ view: UIView, delay: Double, duration: Double) {
         view.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
-        UIView.animate(withDuration: duration, delay: delay, options: .allowUserInteraction, animations: { 
+        UIView.animate(withDuration: duration, delay: delay, options: .allowUserInteraction, animations: {
             view.transform = .identity
         }, completion: nil)
+    }
+    
+    func shakeAnimation(_ view: UIView) {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: view.center.x - 10, y: view.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: view.center.x + 10, y: view.center.y))
+        view.layer.add(animation, forKey: "position")
+    }
+    
+    func hideView(_ view: UIView, withAnimation: Bool) {
+        if withAnimation {
+            UIView.animate(withDuration: 1.0, animations: {
+                view.alpha = 0.0
+            })
+        } else {
+            view.alpha = 0.0
+        }
+    }
+    
+    func showView(_ view: UIView, withAnimation: Bool) {
+        if withAnimation {
+            UIView.animate(withDuration: 1.5, delay: 0.0, options: .allowUserInteraction, animations: {
+                view.alpha = 1.0
+            }, completion: nil)
+        } else {
+            view.alpha = 1.0
+        }
     }
 }
--- AppIdeas/SignInVC.swift
+++ AppIdeas/SignInVC.swift
@@ -7,18 +7,84 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
     
-    
+    @IBOutlet weak var emailPasswordStackView: UIStackView!
+    @IBOutlet weak var emailSignInButton: CustomizedButton!
+    @IBOutlet weak var emailTextField: CustomizedTextField!
+    @IBOutlet weak var passwordTextField: CustomizedTextField!
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var signInButton: CustomizedButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         cancelButton.setImage(#imageLiteral(resourceName: "Cancel"), for: .normal)
+        appIdeasAnimation.hideView(emailPasswordStackView, withAnimation: false)
+        setTextFieldDelegate(for: [emailTextField, passwordTextField])
+        signInButton.disableButtonWithAnimation(1.0, reducingAlphaTo: 0.5)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    @IBAction func emailSignInButtonTapped(_ sender: CustomizedButton) {
+        appIdeasAnimation.hideView(emailSignInButton, withAnimation: true)
+        appIdeasAnimation.showView(emailPasswordStackView, withAnimation: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        if touch.view != emailPasswordStackView {
+            appIdeasAnimation.hideView(emailPasswordStackView, withAnimation: true)
+            appIdeasAnimation.showView(emailSignInButton, withAnimation: true)
+        }
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: CustomizedButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (user, error) in
+            if error != nil {
+                appIdeasAnimation.shakeAnimation((self?.emailPasswordStackView)!)
+            }
+        }
+    }
+    
+    func decideButtonStatus() {
+        emailTextField.backgroundColor == greenForValidInput && passwordTextField.backgroundColor == greenForValidInput ? signInButton.enableButtonWithAnimation(0.5, increasingAlphaTo: 1.0) : signInButton.disableButtonWithAnimation(0.5, reducingAlphaTo: 0.5)
+    }
 }
+
+extension SignInVC: UITextFieldDelegate {
+    
+    func setTextFieldDelegate(for views: [UITextField]) {
+        for view in views {
+            view.delegate = self
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newString = NSString(string: textField.text!).replacingCharacters(in: range, with: string)
+        let sanitizedString = newString.trimmingCharacters(in: .whitespaces)
+        
+        if textField == emailTextField { emailTextField.validateEmailOrPhone(sanitizedString) }
+        else if textField == passwordTextField { passwordTextField.validatePassword(sanitizedString) }
+        
+        decideButtonStatus()
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textField.backgroundColor = redForInvalidInput
+        decideButtonStatus()
+        return true
+    }
+}
+
+
+
+
+
+

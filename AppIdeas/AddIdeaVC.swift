a57cf2d3e9b84e815bd7a2235d09af0ac455a56e
Vandan Patel <VandanPatel@Mains-MacBook-Pro.local>
2017-10-06 22:09:58 -0400

    Implemented dynamic textView for add idea textView.

--- AppIdeas/AddIdeaVC.swift
+++ AppIdeas/AddIdeaVC.swift
@@ -9,31 +9,20 @@
 import UIKit
 
 class AddIdeaVC: UIViewController {
-
+    
     @IBOutlet weak var ideaTextView: CustomizedUITextView!
     @IBOutlet weak var letsGoButton: CustomizedButton!
     @IBOutlet weak var charactersCountLabel: UILabel!
+    @IBOutlet weak var ideaTextViewHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ideaTextView.delegate = self
-        view.addSubview(alertLabel)
-        setupLayoutForLabel()
     }
     
-    let alertLabel: UILabel = {
-        let label = UILabel()
-        label.text = "You have sucessfully created idea"
-        label.textAlignment = .center
-        label.alpha = 0.0
-        return label
-    }()
-    
-    func setupLayoutForLabel() {
-        alertLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        alertLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        alertLabel.heightAnchor.constraint(equalToConstant: 200.0).isActive = true
-        alertLabel.widthAnchor.constraint(equalToConstant: 200.0).isActive = true
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        ideaTextView.setContentOffset(.zero, animated: true)
     }
     
     @IBAction func letsGoTapped(_ sender: CustomizedButton) {
@@ -43,7 +32,7 @@ class AddIdeaVC: UIViewController {
             self?.hideActivityIndicator()
             self?.tabBarController?.selectedIndex = 0
         }
-
+        
     }
 }
 
@@ -52,5 +41,9 @@ extension AddIdeaVC: UITextViewDelegate {
         let newString = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
         charactersCountLabel.text = newString.count <= 150 ? "\(150 - newString.count)" : "0"
         return newString.count <= 150
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        ideaTextViewHeightConstraint.constant = textView.contentSize.height
     }
 }
